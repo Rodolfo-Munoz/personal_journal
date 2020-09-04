a9147409aba7e94671ae1a15e8fa30a7c6471488
mr.verloc <mr.verloc@gmail.com>
2020-09-04 22:52:57 +0100

    styling changes

--- entries/forms.py
+++ entries/forms.py
@@ -1,9 +1,7 @@
 from django.forms import ModelForm
 from .models import Entry
 
-# the classes created here are representations of the html forms and fields
-# just like the classes in models are representations of databases tables and rows
-# I imported the ModelForm and the Entry class from models
+
 
 class EntryForm(ModelForm):
     class Meta:
--- entries/views.py
+++ entries/views.py
@@ -4,18 +4,8 @@ from .forms import EntryForm
 
 
 def index(request):
-
-    # this variable represents the entries in the database
-    # I need to import the Entry class
-    # Entry.objects.all() get all the Entry objects in that class
-    # by changing it to .order_by('-date_posted) they get ordered in descending order, last posted first
     entries = Entry.objects.order_by('-date_posted')
-
-    # context is a dictionary with a key entries and the value is all the entries
-    # in the database. Do I can later pass those values to the index.html
     context = {'entries': entries}
-
-    # so now I can pass the context variable to the template
     return render(request, 'entries/index.html', context)
 
 
